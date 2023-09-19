5353c26b4ce0eeb2720191c7eb8f1828406dab74
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-09-19 15:10:30 +0700

    change

--- src/pages/user/user-list.tsx
+++ src/pages/user/user-list.tsx
@@ -181,7 +181,7 @@ function UserList() {
                         className="block text-gray-700 text-sm font-bold mb-2"
                         htmlFor="password_confirmation"
                       >
-                        password confirmation
+                        Password Confirmation
                       </label>
                       <input
                         className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
